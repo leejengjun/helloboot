415b894dccaa6fe2f462c74c2298477a6e6670fa
GIANTSTEPCORP\jeongjun.lee <youngwoo4215@naver.com>
2023-03-16 11:40:54 +0900

    Spring boot 자세히 살펴보기 실습 코드

--- src/test/java/tobyspring/helloboot/JdbcTemplateTest.java
+++ src/test/java/tobyspring/helloboot/JdbcTemplateTest.java
@@ -4,11 +4,12 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@HellobootTest
+@JdbcTest
 public class JdbcTemplateTest {
     @Autowired
     JdbcTemplate jdbcTemplate;
